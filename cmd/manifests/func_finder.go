ded68aacc70baa17009092f460d5451f59bf537f
Alikhan <alikhan.toleubay@gmail.com>
2023-12-30 13:06:15 +0600

    [Refactor] `Function finder` function
    * Added `init()` to initialise the `funcs` map without passing through
      multiple functions as an argument
    * Separated functions to be more readable

--- cmd/manifests/func_finder.go
+++ cmd/manifests/func_finder.go
@@ -13,11 +13,16 @@ type PathFunc struct {
 	FuncName   string
 }
 
+var funcs map[PathFunc]*ast.FuncDecl
+
+func init() {
+	funcs = map[PathFunc]*ast.FuncDecl{}
+}
+
 // Traverse through the files, and find the functions
 func GetFuncsByFiles(dct map[string]string) (map[PathFunc]*ast.FuncDecl, error) {
-	funcs := map[PathFunc]*ast.FuncDecl{}
-
-	// Add someignore file to ignore most of the non-.go files
+	// TODO: O(N^2), try to make it faster if needed.
+	// Add some ignore file to ignore most of the non-.go files
 	for _, fPath := range dct {
 		set := token.NewFileSet()
 		parsedFiles, err := parser.ParseFile(set, fPath, nil, parser.Mode(0))
@@ -26,17 +31,25 @@ func GetFuncsByFiles(dct map[string]string) (map[PathFunc]*ast.FuncDecl, error) 
 			os.Exit(1)
 		}
 
-		for _, d := range parsedFiles.Decls {
-			if fn, isFn := d.(*ast.FuncDecl); isFn {
-				pf := PathFunc{
-					PathToFile: fPath,
-					FuncName:   fn.Name.Name,
-				}
-
-				funcs[pf] = fn
-			}
-		}
+		populateFuncs(parsedFiles.Decls, fPath)
 	}
 
 	return funcs, nil
+}
+
+func populateFuncs(parsedFilesDecl []ast.Decl, fPath string) {
+	for _, d := range parsedFilesDecl {
+		collectFuncsFromParsedFileDecls(d, fPath)
+	}
+}
+
+func collectFuncsFromParsedFileDecls(declaration ast.Decl, fPath string) {
+	if fn, isFn := declaration.(*ast.FuncDecl); isFn {
+		pf := PathFunc{
+			PathToFile: fPath,
+			FuncName:   fn.Name.Name,
+		}
+
+		funcs[pf] = fn
+	}
 }
